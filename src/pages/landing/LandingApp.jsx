7d1d71ca2837320058640058d8b44a800814ab6b
UrielGGarcia <227o01221@itsx.edu.mx>
2025-09-03 20:07:25 -0600

    hola en h4

--- src/pages/landing/LandingApp.jsx
+++ src/pages/landing/LandingApp.jsx
@@ -4,6 +4,7 @@ function LandingApp() {
         <div className="bg-amber-900 h-300">
             <h1 className="text-white">Tienda moificada</h1>
             <h2>Agrega otro apartado</h2>
+            <h4>hola</h4>
         </div>
     );
 }
