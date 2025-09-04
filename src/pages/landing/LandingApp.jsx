77a8c1c828e4280c89681d19498eddc8bde4b4f6
UrielGGarcia <227o01221@itsx.edu.mx>
2025-09-03 20:03:58 -0600

    Add h2 label

--- src/pages/landing/LandingApp.jsx
+++ src/pages/landing/LandingApp.jsx
@@ -3,6 +3,7 @@ function LandingApp() {
     return (
         <div className="bg-amber-900 h-300">
             <h1 className="text-white">Tienda moificada</h1>
+            <h2>Agrega otro apartado</h2>
         </div>
     );
 }
