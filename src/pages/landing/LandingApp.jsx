210bc9eb828322450d7b3f22be3e21584fc6b2f1
UrielGGarcia <227o01221@itsx.edu.mx>
2025-09-03 19:52:54 -0600

    add h to main div

--- src/pages/landing/LandingApp.jsx
+++ src/pages/landing/LandingApp.jsx
@@ -1,8 +1,8 @@
 
 function LandingApp() {
     return (
-        <div className="bg-amber-900">
-            <h1>Tienda moificada</h1>
+        <div className="bg-amber-900 h-300">
+            <h1 className="text-white">Tienda moificada</h1>
         </div>
     );
 }
